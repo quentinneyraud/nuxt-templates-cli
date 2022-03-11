7fcbdac96833b96f8d9cf7fcf8ec9a4ec9f5786e
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-11 17:36:53 +0100

    Add get-config command and copy to clipboard

--- src/commands/get-config/index.js
+++ src/commands/get-config/index.js
@@ -1,84 +1,23 @@
+const clipboard = require('clipboardy')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const c = require('ansi-colors')
+const Log = require('../../Log')
 
 const run = async _ => {
-  console.log(`
-  import fs, { promises as fsPromises } from 'fs'
-import path from 'path'
-import defu from 'defu'
+  Log.title('nuxt.config.js :')
 
-const ENVIRONMENT = process.env.ENV
-const IS_DEV = ENVIRONMENT === 'dev'
-const IS_PROD = ENVIRONMENT === 'prod'
+  const nuxtConfigFileContent = await fsPromises.readFile(path.resolve(__dirname, 'nuxt.config.js'), { encoding: 'utf8' })
 
-/**
+  console.log(c.cyan(nuxtConfigFileContent))
 
-  Returns an array of each features config
+  Log.blankLine()
+  Log.blankLine()
 
- */
-const getFeaturesConfigs = async (_) => {
-  const CONFIGS_PATH = path.resolve(__dirname, 'configs')
+  clipboard.writeSync(nuxtConfigFileContent)
 
-  if (!fs.existsSync(CONFIGS_PATH)) {
-    return []
-  }
-
-  const configsFilesNames = await fsPromises.readdir(CONFIGS_PATH)
-
-  return configsFilesNames
-    .map((configFileName) => {
-      return require(path.resolve(CONFIGS_PATH, configFileName))({
-        IS_DEV,
-        IS_PROD
-      })
-    })
-}
-
-export default async (_) => {
-  const baseConfig = {
-    target: 'static',
-
-    head: {
-      title: 'test-nuxt-templates-cli',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ]
-    },
-    css: [],
-    plugins: [],
-    components: true,
-    buildModules: [
-      '@nuxtjs/eslint-module'
-    ],
-    modules: [
-      '@nuxtjs/pwa'
-    ],
-    pwa: {
-      manifest: {
-        lang: 'en'
-      }
-    },
-    build: {}
-  }
-
-  const config = defu(baseConfig, ...(await getFeaturesConfigs()))
-
-  /**
-
-    Override config here
-
-   */
-
-  // ...
-
-  return config
-}
-
-  `)
+  Log.log('🎉 nuxt.config.js content is copied in your clipboard !')
 }
 
 module.exports = {
